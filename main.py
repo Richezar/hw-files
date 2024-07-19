df1a78c94d7613b483a3f189892c5593715460c2
Nikita Fomenko <fomenkoper@gmail.com>
2024-07-19 22:50:21 +0300

    change main

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-my_cook_book = {}
-from copy import deepcopy
 import os
+def create_cook_book():
+    res = {}
+    with open('recipes.txt', encoding='utf-8') as recept:
+        for i in recept:
+            dish = i[:-1]
+            ingredient_quantity = int(recept.readline())
+            tmp = []
+            for j in range(ingredient_quantity):
+                a, b, c = recept.readline().strip().split(' | ')
+                tmp.append({'ingredient_name' : a, 'quantity' : int(b), 'measure' : c})
+                res[dish] = tmp
+            fakeline = recept.readline()
+        return res
+
 def get_shop_list_by_dishes(dishes, person_count):
     res = {}
-    cook_book = deepcopy(my_cook_book)
+    cook_book = create_cook_book()
     for i in dishes:
         if i in cook_book:
             for j in cook_book[i]:
@@ -13,35 +25,28 @@ def get_shop_list_by_dishes(dishes, person_count):
             print(f'Блюда {i} нет в рецептах')
     return print(res)
 
-with open('recipes.txt', encoding='utf-8') as recept:
-    for i in recept:
-        dish = i[:-1]
-        ingredient_quantity = int(recept.readline())
-        tmp = []
-        for j in range(ingredient_quantity):
-            a, b, c = recept.readline().strip().split(' | ')
-            tmp.append({'ingredient_name' : a, 'quantity' : int(b), 'measure' : c})
-            my_cook_book[dish] = tmp
-        fakeline = recept.readline()
-
-
 get_shop_list_by_dishes(['Омлет', 'Омлет'], 2)
+get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2)
 
-file = {}
-for filename in sorted(os.listdir('sorted'), key=len):
-    with open(f'sorted/{filename}', encoding='utf-8') as f:
-        tmp = []
-        for i in f:
-            tmp.append(i)
-        file[filename, len(tmp)] = tmp
+def reading_all_files():
+    res = {}
+    for filename in sorted(os.listdir('sorted'), key=len):
+        with open(f'sorted/{filename}', encoding='utf-8') as f:
+            tmp = []
+            for i in f:
+                tmp.append(i)
+            res[filename, len(tmp)] = tmp
+    return res
 
-with open('sort_files.txt', 'w', encoding='utf-8') as files:
-    for k, v in sorted(file.items(), key=lambda x: x[1], reverse=True):
-        for i in k:
-            files.write(str(i) + '\n')
-        n = 1
-        for i in v:
-            files.write(str(n) + ' ' + i)
-            n += 1
-        files.write('\n')
+def sorted_all_files():
+    with open('sort_files.txt', 'w', encoding='utf-8') as files:
+        for k, v in sorted(reading_all_files().items(), key=lambda x: x[1], reverse=True):
+            for i in k:
+                files.write(str(i) + '\n')
+            n = 1
+            for i in v:
+                files.write(str(n) + ' ' + i)
+                n += 1
+            files.write('\n')
 
+sorted_all_files()
